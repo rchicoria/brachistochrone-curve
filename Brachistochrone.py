1556aa5c785541958194c8e3a6f95f2855a344aa
Ricardo Lopes <ricardorpl@gmail.com>
2011-05-20 18:32:54 +0100

    já é computação evolutiva como deve ser. Faltam pormenores como crossover, estatísticas, etc.

--- Brachistochrone.py
+++ Brachistochrone.py
@@ -66,9 +66,41 @@ def mutacao(individuo,y1):
     novos_cromossomas.extend(cromossomas[i+1:])
     return [novos_cromossomas,0]
 
-def sga(x1, y1, x2, y2, ngeracoes, nindividuos, ngenes, seleccao_pais, fitness, func_mutacao,
-		seleccao_sobreviventes, tamanho_torneio, prob_crossover, prob_mutacao, tamanho_elite):
-		
+# escolhe os sobreviventes através de elitismo
+def elitismo(populacao, descendentes, tamanho_elite):
+    tamanho = int(len(populacao) * tamanho_elite)
+    nova_populacao = populacao[:tamanho] + descendentes[:len(populacao) - tamanho]
+    return nova_populacao
+
+if __name__ == '__main__':
+
+	# ponto 1
+	sys.stdout.write("x1 y1 = ");
+	x1, y1 = map(int, sys.stdin.readline().split())
+	
+	# ponto 2
+	sys.stdout.write("x2 y2 = ");
+	x2, y2 = map(int, sys.stdin.readline().split())
+	
+	# número de gerações
+	sys.stdout.write("Número de gerações = ");
+	ngeracoes = int(sys.stdin.readline())
+	
+	# número de indivíduos
+	sys.stdout.write("Número de indivíduos = ");
+	nindividuos = int(sys.stdin.readline())
+	
+	# número de genes
+	sys.stdout.write("Número de genes = ");
+	ngenes = int(sys.stdin.readline())
+	
+	# outras variáveis
+	tamanho_torneio = 3
+	prob_crossover = 0.3
+	prob_mutacao = 0.5
+	tamanho_elite = 0.3
+	
+	# faz os cálculos
 	# cria a população
 	populacao = cria_populacao(x1, y1, x2, y2, nindividuos, ngenes)
 	# avalia a população
@@ -87,28 +119,8 @@ def sga(x1, y1, x2, y2, ngeracoes, nindividuos, ngenes, seleccao_pais, fitness, 
 		# avalia os descendentes
 		descendentes = [[individuo[0], calcBrachTime(individuo[0])] for individuo in descendentes]
 		descendentes.sort(key=itemgetter(1))
-		print "Melhor descendente da geração %d: %f" % (geracao, descendentes[0][1])
-	# continua...
-	
-	# só merda a partir daqui
+		# selecciona sobreviventes
+		populacao = elitismo(populacao, descendentes, tamanho_elite)
+		populacao.sort(key=itemgetter(1))
+		sys.stdout.write( "Melhor descendente da geração %d: %f\n" % (geracao, populacao[0][1]) )
 	grafico(populacao[0][0])
-	return True
-
-if __name__ == '__main__':
-	# ponto 1
-	sys.stdout.write("x1 y1 = ");
-	x1, y1 = map(int, sys.stdin.readline().split())
-	# ponto 2
-	sys.stdout.write("x2 y2 = ");
-	x2, y2 = map(int, sys.stdin.readline().split())
-	# número de gerações
-	sys.stdout.write("Número de gerações = ");
-	ngeracoes = int(sys.stdin.readline())
-	# número de indivíduos
-	sys.stdout.write("Número de indivíduos = ");
-	nindividuos = int(sys.stdin.readline())
-	# número de genes
-	sys.stdout.write("Número de genes = ");
-	ngenes = int(sys.stdin.readline())
-	# faz os cálculos
-	sga(x1, y1, x2, y2, ngeracoes, nindividuos, ngenes,0,0,0,0,3,0,0.5,0)
