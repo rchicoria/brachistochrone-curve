19ca511fc41f3fca18a20f586a1af0e9a92ec94b
Ricardo Lopes <ricardorpl@gmail.com>
2011-05-20 17:49:53 +0100

    já são seleccionados progenitores e descendentes para cada geração

--- Brachistochrone.py
+++ Brachistochrone.py
@@ -3,7 +3,8 @@
 from BrachFitness import *
 import sys
 from matplotlib.pyplot import plot, show
-from random import uniform
+from random import uniform, sample, random, choice
+from operator import itemgetter
 
 """
 Trabalho Prático Nº2: Curva Braquistócrona
@@ -21,6 +22,7 @@ Ricardo Lopes
 Rui Chicória
 """
 
+# recebe os pontos da curva de um indivíduo, e traça o gráfico respectivo
 def grafico(curva):
 	x = []
 	y = []
@@ -32,6 +34,7 @@ def grafico(curva):
 	plot(x, y)
 	show()
 
+# cria um indivíduo de acordo com as regras exigidas
 def cria_individuo(x1, y1, x2, y2, ngenes):
 	individuo = [x1, y1]
 	for i in xrange(ngenes-2):
@@ -41,19 +44,50 @@ def cria_individuo(x1, y1, x2, y2, ngenes):
 	individuo.append(y2)
 	return [individuo, 0]
 
+# cria uma população com o número de indivíduos exigido
 def cria_populacao(x1, y1, x2, y2, nindividuos, ngenes):
 	return [cria_individuo(x1, y1, x2, y2, ngenes) for i in xrange(nindividuos)]
 
-def cria_eixo_x(x1, x2, ngenes):
-	return [(x1+((x2-x1)*(float(i)/(ngenes-1)))) for i in xrange(ngenes)]
+# selecciona os progenitores de uma dada geração através do método de torneio
+def seleccao_torneio(populacao, tamanho_torneio):
+    torneio = sample(populacao, tamanho_torneio)
+    torneio.sort(key=itemgetter(1))
+    return torneio[0]
 
-def sga(x1, y1, x2, y2, ngeracoes, nindividuos, ngenes, seleccao_pais, fitness, mutacao,
-		seleccao_sobreviventes, tamanho_torneio, prob_mutacao, tamanho_elite, tamanho_mutacao):
+# cria novos descendentes através do método de mutação de genes
+def mutacao(individuo,y1):
+    cromossomas = individuo[0]
+    i = 1+2+2*choice(xrange((len(cromossomas)-4)/2))
+    novo_gene = uniform(0, y1)
+    while novo_gene == cromossomas[i]:
+        novo_gene = uniform(0, y1)
+    novos_cromossomas = cromossomas[:i]
+    novos_cromossomas.append(novo_gene)
+    novos_cromossomas.extend(cromossomas[i+1:])
+    return [novos_cromossomas,0]
+
+def sga(x1, y1, x2, y2, ngeracoes, nindividuos, ngenes, seleccao_pais, fitness, func_mutacao,
+		seleccao_sobreviventes, tamanho_torneio, prob_crossover, prob_mutacao, tamanho_elite):
 		
 	# cria a população
 	populacao = cria_populacao(x1, y1, x2, y2, nindividuos, ngenes)
 	# avalia a população
 	populacao = [[individuo[0], calcBrachTime(individuo[0])] for individuo in populacao]
+	for geracao in xrange(ngeracoes):
+		# selecciona os progenitores
+		progenitores = [seleccao_torneio(populacao, tamanho_torneio) for i in xrange(nindividuos)]
+		# cria descendentes
+		descendentes = []
+		# para já, apenas permite por mutação
+		for i in xrange(nindividuos):
+			if random() < prob_mutacao:
+				descendentes.append(mutacao(progenitores[i],y1))
+			else:
+				descendentes.append(progenitores[i])
+		# avalia os descendentes
+		descendentes = [[individuo[0], calcBrachTime(individuo[0])] for individuo in descendentes]
+		descendentes.sort(key=itemgetter(1))
+		print "Melhor descendente da geração %d: %f" % (geracao, descendentes[0][1])
 	# continua...
 	
 	# só merda a partir daqui
@@ -77,4 +111,4 @@ if __name__ == '__main__':
 	sys.stdout.write("Número de genes = ");
 	ngenes = int(sys.stdin.readline())
 	# faz os cálculos
-	sga(x1, y1, x2, y2, ngeracoes, nindividuos, ngenes,0,0,0,0,0,0,0,0)
+	sga(x1, y1, x2, y2, ngeracoes, nindividuos, ngenes,0,0,0,0,3,0,0.5,0)
